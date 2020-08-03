e3a7cc33066d6996052693bf90fa84ce3977f69f
fabioabreureis <fabioabreureis@gmail.com>
2020-08-03 17:59:04 -0300

    update openstack util

--- openstack_util.py
+++ openstack_util.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import json
-import openstack.cloud
-import configparser
 from measures import Hypervisor
-from influx import influx_connection
+import os
 
-#openstack.enable_logging(http_debug=True)
+os.environ['REQUESTS_CA_BUNDLE'] = '/etc/ssl/certs/rootCA.pem'
 
 class OpenstackUtil:
     def __init__(self, cloud_name):
@@ -14,16 +9,13 @@ class OpenstackUtil:
         self.conn = openstack.connect(cloud=self.cloud_name)
 
     def get_hypervisors(self):
-        for h in self.conn.list_hypervisors():
             hypervisors = []
             settings = configparser.ConfigParser()
             settings.read('openstack_influx.ini')
             cpu_ratio = settings.get('compute', 'cpu_ratio')
-            hypervisors.append(Hypervisor(cloud_name=self.cloud_name,compute=h['name'],status=h['status'],running_vms=h['running_vms'],vcpus_used=h['vcpus_used'],memory_used=h['memory_used'],vcpus_size=h['vcpus'] * cpu_ratio,memory_size=h['memory_size']).to_measurement())
+            for h in self.conn.list_hypervisors(): 
+                    hypervisors.append(Hypervisor(cloud_name=self.cloud_name,compute=h['name'],status=h['status'],running_vms=h['running_vms'],vcpus_used=h['vcpus_used'],memory_used=h['memory_used'],vcpus_size=h['vcpus'],memory_size=h['memory_size']).to_measurement()) 
             return hypervisors
-
-
-
 
 
 
